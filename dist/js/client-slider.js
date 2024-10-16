c2a57ef506263edf69814c7049b7f7917196edcd
bullets012 <lancegenesismercado0111@gmail.com>
2024-10-16 12:27:43 +0800

    hehe

---
+++ dist/js/client-slider.js
@@ -0,0 +1,10 @@
+var slider = tns({
+  container: ".clients__slider",
+  items: 1,
+  speed: 300,
+  slideBy: "page",
+  autoplay: true,
+  controls: false,
+  nav: false,
+  autoplayButtonOutput: false,
+});
